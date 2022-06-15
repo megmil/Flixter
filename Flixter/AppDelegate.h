6ed4c23a056d9f46934dc26fa1794ecb091bcb56
megmil <megmil@umich.edu>
2022-06-15 10:25:16 -0700

    Initial Commit

---
+++ Flixter/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Flixter
+//
+//  Created by Megan Miller on 6/15/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
